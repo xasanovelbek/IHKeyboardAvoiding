ec68e223fe3e85b2842ffddf0f9e6ba21e046467
Elbek Khasanov <94638543+xasanovelbek@users.noreply.github.com>
2023-05-29 12:14:00 +0500

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -1,5 +1,14 @@
-import PackageDescription
+// swift-tools-version:5.0
 
+import PackageDescription
+let name = "IHKeyboardAvoiding"
 let package = Package(
-    name: "IHKeyboardAvoiding"
+    name: name,
+    products: [
+        .library(name: "IHKeyboardAvoiding", targets: ["IHKeyboardAvoiding"])
+    ],
+    targets: [
+        .target(name: "IHKeyboardAvoiding", path: "IHKeyboardAvoiding"),
+    ],
+    swiftLanguageVersions: [.v5]
 )
